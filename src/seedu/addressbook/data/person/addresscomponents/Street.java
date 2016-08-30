f079102ec1769c80b1061e784d15bf7c5d60f9b4
Xien Dong Wang <xdrawks@gmail.com>
2016-08-30 09:23:14 +0800

    Added private String variable, street in Street class

--- src/seedu/addressbook/data/person/addresscomponents/Street.java
+++ src/seedu/addressbook/data/person/addresscomponents/Street.java
@@ -4,5 +4,7 @@ package seedu.addressbook.data.person.addresscomponents;
  * Represents a Person's address component (street) in the address book.
  */
 public class Street {
-
+	
+	private String street;
+	
 }
