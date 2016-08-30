8376d0978df983fb8c8bb7c3c0c2525b482402a3
Xien Dong Wang <xdrawks@gmail.com>
2016-08-30 09:42:41 +0800

    Added constructor for Unit class

--- src/seedu/addressbook/data/person/addresscomponents/Unit.java
+++ src/seedu/addressbook/data/person/addresscomponents/Unit.java
@@ -9,4 +9,11 @@ public class Unit {
     private String levelValue;
     private String doorNumberValue;
     
+    /**
+     * Constructs an address component, unit, with level and doorNumber.
+     */
+    public Unit (String level, String doorNumber){
+        this.levelValue = level;
+        this.doorNumberValue = doorNumber;
+    }
 }
