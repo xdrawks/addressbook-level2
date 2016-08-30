c165b7aa104bd68cdfdf17abff20c88fd2a5cdc3
Xien Dong Wang <xdrawks@gmail.com>
2016-08-30 09:32:17 +0800

    Changed tab indentation to 4 spaces in Block class

--- src/seedu/addressbook/data/person/addresscomponents/Block.java
+++ src/seedu/addressbook/data/person/addresscomponents/Block.java
@@ -4,20 +4,20 @@ package seedu.addressbook.data.person.addresscomponents;
  * Represents a Person's address component (block) in the address book.
  */
 public class Block {
-	
-	private String block;
-	
-	/**
-	 * Constructs an address component, block
-	 */
-	public Block (String block){
-		this.block = block;
-	}
-	
-	/**
-	 * Gets the block String in this class.
-	 */
-	public String getBlock(){
-		return this.block;
-	}
+    
+    private String block;
+    
+    /**
+     * Constructs an address component, block
+     */
+    public Block (String block){
+        this.block = block;
+    }
+    
+    /**
+     * Gets the block String in this class.
+     */
+    public String getBlock(){
+        return this.block;
+    }
 }
