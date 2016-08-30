abb10dd56d3e32e44b650f4f5878e62ef9aef861
Xien Dong Wang <xdrawks@gmail.com>
2016-08-30 09:31:42 +0800

    Changed tab indentation to spaces in Street class

--- src/seedu/addressbook/data/person/addresscomponents/Street.java
+++ src/seedu/addressbook/data/person/addresscomponents/Street.java
@@ -4,13 +4,13 @@ package seedu.addressbook.data.person.addresscomponents;
  * Represents a Person's address component (street) in the address book.
  */
 public class Street {
-	
-	private String street;
-	
-	/**
-	 * Constructs an address component, Street
-	 */
-	public Street (String street){
-		this.street = street;	
-	}
+    
+    private String street;
+    
+    /**
+     * Constructs an address component, Street
+     */
+    public Street (String street){
+        this.street = street;    
+    }
 }
