d68eac9929efdbae9b2dbd5ed760b663055afc36
Xien Dong Wang <xdrawks@gmail.com>
2016-08-30 09:47:19 +0800

    Added postalCodeValue field in PostalCode class

--- src/seedu/addressbook/data/person/addresscomponents/PostalCode.java
+++ src/seedu/addressbook/data/person/addresscomponents/PostalCode.java
@@ -4,5 +4,6 @@ package seedu.addressbook.data.person.addresscomponents;
  * Represents a Person's address component (postal code) in this address book
  */
 public class PostalCode {
-
+    
+    private String postalCodeValue;
 }
