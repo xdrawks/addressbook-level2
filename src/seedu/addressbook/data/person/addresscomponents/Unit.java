ee50da6196d71a1e4b545fc53d74b7806d793b76
Xien Dong Wang <xdrawks@gmail.com>
2016-08-30 09:38:47 +0800

    Added comments for Unit Class

--- src/seedu/addressbook/data/person/addresscomponents/Unit.java
+++ src/seedu/addressbook/data/person/addresscomponents/Unit.java
@@ -1,5 +1,9 @@
 package seedu.addressbook.data.person.addresscomponents;
 
+/**
+ * Represents a Person's address component (unit) in the address book
+ * Where unit contains both level and door number.
+ */
 public class Unit {
 
 }
