5d4e2e014bd63eccc529c1b1fd3bb2d2482dc736
Xien Dong Wang <xdrawks@gmail.com>
2016-09-03 20:17:35 +0800

    Added constructor for Contact

--- src/seedu/addressbook/data/person/Contact.java
+++ src/seedu/addressbook/data/person/Contact.java
@@ -4,5 +4,10 @@ public class Contact {
     
     public final String value;
     private boolean isPrivate;
+    
+    public Contact (String value, boolean isPrivate) {
+        this.value = value;
+        this.isPrivate = isPrivate;
+    }
 
 }
