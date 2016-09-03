2aa696001f0989235310d1ac607478664d8f2abb
Xien Dong Wang <xdrawks@gmail.com>
2016-09-03 23:58:59 +0800

    Added testOneNull

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -6,4 +6,9 @@ public class UtilsTest {
     public void testNoNull() {   
         assertEquals(Utils.isAnyNull(1, 2, "Happy", 2.34, true, false, 3, 4, 5, 6, 7, 8, 9, 0), false);
     }
+    
+    @Test
+    public void testOneNull() {   
+        assertEquals(Utils.isAnyNull("well", 12, "well", "13", null, 15.6), true);
+    }
 }
