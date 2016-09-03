130d1ed349928257a8ef9b7809cb4228a5d488ce
Xien Dong Wang <xdrawks@gmail.com>
2016-09-03 23:20:19 +0800

    Added constructor for Tagging

--- src/seedu/addressbook/data/person/Tagging.java
+++ src/seedu/addressbook/data/person/Tagging.java
@@ -5,5 +5,11 @@ public class Tagging {
     private boolean isAdded; //If this value is false, it means the tag has been deleted.
     private Person person; //Person being associated with this tag
     private Tag tag; //Tag being associated with this person.
+    
+    public Tagging (Person person, Tag tag, boolean isAdded) {
+        this.isAdded = isAdded;
+        this.person = person;
+        this.tag = tag;
+    }
 
 }
