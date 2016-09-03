224c76dce5336d35e8682c40ba0f90e0c49c5848
Xien Dong Wang <xdrawks@gmail.com>
2016-09-03 23:20:04 +0800

    Added isAdded, person and tag

--- src/seedu/addressbook/data/person/Tagging.java
+++ src/seedu/addressbook/data/person/Tagging.java
@@ -1,5 +1,9 @@
 package seedu.addressbook.data.person;
 
 public class Tagging {
+    
+    private boolean isAdded; //If this value is false, it means the tag has been deleted.
+    private Person person; //Person being associated with this tag
+    private Tag tag; //Tag being associated with this person.
 
 }
