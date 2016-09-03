95c55f9456d9214f8cde2b9544cdc8aa16468131
Xien Dong Wang <xdrawks@gmail.com>
2016-09-03 23:58:50 +0800

    Added testNoNull

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -2,4 +2,8 @@ package seedu.addressbook.common;
 
 public class UtilsTest {
     
+    @Test
+    public void testNoNull() {   
+        assertEquals(Utils.isAnyNull(1, 2, "Happy", 2.34, true, false, 3, 4, 5, 6, 7, 8, 9, 0), false);
+    }
 }
